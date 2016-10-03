cee5e0c2560b8e5172d02f36f7f385b4b6de2689
Alyson Deives <raziel363@gmail.com>
2016-10-02 22:27:35 -0300

    conflict

--- apps/jacobi/jacobi_acc_parallel.cpp
+++ apps/jacobi/jacobi_acc_parallel.cpp
@@ -20,15 +20,9 @@ inline void stencilKernel(float* __restrict__ input, float* __restrict__ output,
 	{
 	for (int t = 0; t < T_MAX; t++){
 
-	#pragma acc parallel loop tile(32,32) device_type(nvidia) stencil 
+	#pragma acc parallel loop  
 	for (int y = 1; y < height - 1; y++){
-		//#pragma acc loop 
-		for (int x = 1; x < width - 1; x++){
-				output[y*width+x] = 0.25f * (input[(y+1)*width + x] + input[(y-1)*width + x] +
-				                   	     input[y*width + (x+1)] + input[y*width + (x-1)] - beta);
-		#pragma acc parallel loop independent
-		for (int y = 0; y < height; y++){
-			for (int x = 0; x < width; x++){
+		for (int x = 0; x < width; x++){
 				/*	Corner 1	*/
                 if ( (y == 0) && (x == 0) ) {
                     output[y*width+x] = 0.25f * (input[(y+1)*width + x] + 
@@ -79,16 +73,12 @@ inline void stencilKernel(float* __restrict__ input, float* __restrict__ output,
 												input[(y-1)*width + x] +
 												input[y*width + (x+1)] +
 												input[y*width + (x-1)] - beta);
-				}
-			}
+		}
+		}
 		} 
 		
 		//swap data
-		if(t>1 & t<T_MAX - 1){
-
-			#pragma acc parallel loop tile(32,32)  
-			for (int y = 1; y < height - 1; y++){
-				//#pragma acc loop 
+		if(t>1 & t<T_MAX - 1){	 
 			#pragma acc parallel loop independent 
 			for (int y = 1; y < height - 1; y++){
 				for (int x = 1; x < width - 1; x++){
