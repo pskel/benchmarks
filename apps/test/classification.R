5e7267ae77e2e77bff138e930232c3d82b6083cb
Alyson Deives <raziel363@gmail.com>
2017-02-23 09:18:27 -0300

    awp

--- apps/test/classification.R
+++ apps/test/classification.R
@@ -181,6 +181,13 @@ colnames(df)[which(names(df) == "ite")] <- "ITERATIONS"
 colnames(df)[which(names(df) == "L2_DCM")] <- "CPU_L2_DCM"
 colnames(df)[which(names(df) == "L3_TCM")] <- "CPU_L3_TCM"
 
+colnames(apps)[which(names(apps) == "l2_l1_read_hit_rate")] <- "GPU_L2_TCH"
+colnames(apps)[which(names(apps) == "l1_cache_global_hit_rate")] <- "GPU_L1_TCH"
+colnames(apps)[which(names(apps) == "sm_efficiency")] <- "GPU_SME"
+colnames(apps)[which(names(apps) == "ite")] <- "ITERATIONS"
+colnames(apps)[which(names(apps) == "L2_DCM")] <- "CPU_L2_DCM"
+colnames(apps)[which(names(apps) == "L3_TCM")] <- "CPU_L3_TCM"
+
 
 df = df[which(df$numAdd==1 & df$numMult==1),]
 
@@ -271,7 +278,7 @@ df$dram_utilization = as.integer(df$dram_utilization)
 
 #ite + l1_cache_global_hit_rate + BR_INS + FP_INS + L2_DCM + L3_TCM + l2_l1_read_hit_rate +  branch_efficiency + l2_utilization + FP_INS + LD_INS + achieved_occupancy + alu_fu_utilization + ldst_fu_utilization + dram_utilization + l1_shared_utilization + flop_sp_efficiency + sm_efficiency
 
-fit4 <- rpart(pct_oracle ~ ite + l1_cache_global_hit_rate + L2_DCM + L3_TCM + l2_l1_read_hit_rate + sm_efficiency,method="class", data=df,control=rpart.control(minsplit=10,minbucket=3))
+fit4 <- rpart(pct_oracle ~ ITERATIONS + GPU_L1_TCH + GPU_L2_TCH + CPU_L2_DCM + CPU_L3_TCM ,method="class", data=df,control=rpart.control(minsplit=5,minbucket=3))
 
 #prune
 pfit<- prune(fit4, cp=fit4$cptable[which.min(fit4$cptable[,"xerror"]),"CP"])
@@ -280,14 +287,14 @@ predictions_fit = predict(pfit,apps,type="class")
 predictions_pfit = predict(fit4,apps,type="class")
 print(predictions_fit)
 print(predictions_pfit)
-#setEPS()
-#postscript("prp_tree_quadro.eps")
+setEPS()
+postscript("prp_tree_quadro2b.eps")
 
 x <- prp(pfit,type=0,extra=102,digits=4,under=FALSE,faclen=0,varlen=0,split.border.col=1,fallen.leaves = TRUE,leaf.round=1.8,ycompress = TRUE,compress=TRUE,xcompact=FALSE,xcompact.ratio=0.5,ycompact=FALSE,nn=FALSE,split.font=0.9,branch=1,lt="\n< ",ge="\n>= ",xflip=TRUE,cex=0.6,ycompress.cex=Inf,accept.cex=0,Fallen.yspace=0.05,trace=TRUE,nn.cex=0.7)
 
-x <- prp(fit4,type=0,extra=102,digits=4,under=FALSE,faclen=0,varlen=0,split.border.col=1,fallen.leaves = TRUE,leaf.round=1.8,ycompress = TRUE,compress=TRUE,xcompact=FALSE,xcompact.ratio=0.5,ycompact=FALSE,nn=FALSE,split.font=0.9,branch=1,lt="\n< ",ge="\n>= ",xflip=TRUE,cex=0.6,ycompress.cex=Inf,accept.cex=0,Fallen.yspace=0.05,trace=TRUE,nn.cex=0.7)
+#x <- prp(fit4,type=0,extra=102,digits=4,under=FALSE,faclen=0,varlen=0,split.border.col=1,fallen.leaves = TRUE,leaf.round=1.8,ycompress = TRUE,compress=TRUE,xcompact=FALSE,xcompact.ratio=0.5,ycompact=FALSE,nn=FALSE,split.font=0.9,branch=1,lt="\n< ",ge="\n>= ",xflip=TRUE,cex=0.6,ycompress.cex=Inf,accept.cex=0,Fallen.yspace=0.05,trace=TRUE,nn.cex=0.7)
 
-#dev.off()
+dev.off()
 
 accuracy.global = sum(predictions4==apps$pct_gpu)/nrow(apps)
 print(accuracy.global)
