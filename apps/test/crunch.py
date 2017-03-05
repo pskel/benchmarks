defa066b7998e66981e69271583417383293fe21
Alyson Deives Pereira <raziel363@gmail.com>
2017-03-04 22:05:11 -0300

    update

--- apps/test/crunch.py
+++ apps/test/crunch.py
@@ -422,13 +422,13 @@ header = list()
 metrics_time = list()
 for metric in kv_time:
   metrics_time.append(metric)
-  #header.append(kv_time[metric])
+  header.append(kv_time[metric])
 
 
 metrics_prof = list()
 for metric in kv_prof:
   metrics_prof.append(metric)
-  header.append(kv_prof[metric])
+  #header.append(kv_prof[metric])
 
 #sort the header by metric name
 header.sort()
@@ -564,18 +564,18 @@ prof_files.sort()
 
 # extract the results from the files
 results = list()
-for i in range(len(prof_files)):
+for i in range(len(time_files)):
   # extract configuration attributes from the file name
-  data = get_attr_from_filename(prof_files[i])
+  data = get_attr_from_filename(time_files[i])
 
   # from the "time" files, we want PS: also needs to take this for prof
-  #for metric in metrics_time:
-    #data += [ mean(extract_values(metric, time_files[i])) ]
+  for metric in metrics_time:
+    data += [ mean(extract_values(metric, time_files[i])) ]
 
   # for the "prof" files, we want all metrics
   #if data[3] > '0' :
-  for metric in metrics_prof:
-	data += [ mean(extract_values(metric, prof_files[i])) ]
+  #for metric in metrics_prof:
+	#data += [ mean(extract_values(metric, prof_files[i])) ]
   #k += 1
 	
   # create a results row
